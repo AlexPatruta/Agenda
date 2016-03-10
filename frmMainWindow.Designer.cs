a1eb2549602e07edd9ac7ad1a611d8dacf6c5c73
Florin Patruta <Florin Patruta>
2016-03-10 12:52:19 +0200

    Removed the database

--- frmMainWindow.Designer.cs
+++ frmMainWindow.Designer.cs
@@ -45,9 +45,9 @@
             // lblFilter
             // 
             this.lblFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFilter.Location = new System.Drawing.Point(16, 8);
+            this.lblFilter.Location = new System.Drawing.Point(16, 21);
             this.lblFilter.Name = "lblFilter";
-            this.lblFilter.Size = new System.Drawing.Size(56, 32);
+            this.lblFilter.Size = new System.Drawing.Size(52, 29);
             this.lblFilter.TabIndex = 0;
             this.lblFilter.Text = "Filtru";
             this.lblFilter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -55,7 +55,8 @@
             // tbInputFilter
             // 
             this.tbInputFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbInputFilter.Location = new System.Drawing.Point(72, 8);
+            this.tbInputFilter.Location = new System.Drawing.Point(74, 21);
+            this.tbInputFilter.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.tbInputFilter.Name = "tbInputFilter";
             this.tbInputFilter.Size = new System.Drawing.Size(232, 29);
             this.tbInputFilter.TabIndex = 1;
@@ -65,7 +66,8 @@
             // 
             // btnApplyFilter
             // 
-            this.btnApplyFilter.Location = new System.Drawing.Point(312, 8);
+            this.btnApplyFilter.Location = new System.Drawing.Point(312, 21);
+            this.btnApplyFilter.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.btnApplyFilter.Name = "btnApplyFilter";
             this.btnApplyFilter.Size = new System.Drawing.Size(83, 29);
             this.btnApplyFilter.TabIndex = 2;
@@ -77,9 +79,10 @@
             // 
             this.dgViewAgenda.BackgroundColor = System.Drawing.Color.White;
             this.dgViewAgenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgViewAgenda.Location = new System.Drawing.Point(16, 40);
+            this.dgViewAgenda.Location = new System.Drawing.Point(16, 62);
+            this.dgViewAgenda.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.dgViewAgenda.Name = "dgViewAgenda";
-            this.dgViewAgenda.Size = new System.Drawing.Size(376, 287);
+            this.dgViewAgenda.Size = new System.Drawing.Size(376, 442);
             this.dgViewAgenda.TabIndex = 3;
             this.dgViewAgenda.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgViewAgenda_CellContentClick);
             this.dgViewAgenda.Click += new System.EventHandler(this.dgViewAgenda_Click);
@@ -88,7 +91,7 @@
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(394, 36);
+            this.lblName.Location = new System.Drawing.Point(394, 55);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(62, 24);
             this.lblName.TabIndex = 4;
@@ -98,7 +101,7 @@
             // 
             this.lblTelephone.AutoSize = true;
             this.lblTelephone.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTelephone.Location = new System.Drawing.Point(394, 86);
+            this.lblTelephone.Location = new System.Drawing.Point(394, 132);
             this.lblTelephone.Name = "lblTelephone";
             this.lblTelephone.Size = new System.Drawing.Size(79, 24);
             this.lblTelephone.TabIndex = 5;
@@ -107,10 +110,11 @@
             // tbName
             // 
             this.tbName.AcceptsTab = true;
-            this.tbName.Location = new System.Drawing.Point(398, 63);
+            this.tbName.Location = new System.Drawing.Point(398, 97);
+            this.tbName.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.tbName.MaxLength = 50;
             this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(293, 20);
+            this.tbName.Size = new System.Drawing.Size(293, 26);
             this.tbName.TabIndex = 6;
             this.tbName.Text = "Introduceţi numele aici";
             this.tbName.Click += new System.EventHandler(this.tbName_Click);
@@ -119,10 +123,11 @@
             // tbTelephone
             // 
             this.tbTelephone.AcceptsReturn = true;
-            this.tbTelephone.Location = new System.Drawing.Point(398, 113);
+            this.tbTelephone.Location = new System.Drawing.Point(398, 174);
+            this.tbTelephone.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.tbTelephone.MaxLength = 10;
             this.tbTelephone.Name = "tbTelephone";
-            this.tbTelephone.Size = new System.Drawing.Size(293, 20);
+            this.tbTelephone.Size = new System.Drawing.Size(293, 26);
             this.tbTelephone.TabIndex = 7;
             this.tbTelephone.Text = "Introduceţi numărul de telefon aici";
             this.tbTelephone.WordWrap = false;
@@ -131,9 +136,10 @@
             // 
             // btnAddEntry
             // 
-            this.btnAddEntry.Location = new System.Drawing.Point(398, 280);
+            this.btnAddEntry.Location = new System.Drawing.Point(398, 431);
+            this.btnAddEntry.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.btnAddEntry.Name = "btnAddEntry";
-            this.btnAddEntry.Size = new System.Drawing.Size(75, 48);
+            this.btnAddEntry.Size = new System.Drawing.Size(75, 74);
             this.btnAddEntry.TabIndex = 8;
             this.btnAddEntry.Text = "Adaugă";
             this.btnAddEntry.UseVisualStyleBackColor = true;
@@ -141,9 +147,10 @@
             // 
             // btnRemoveEntry
             // 
-            this.btnRemoveEntry.Location = new System.Drawing.Point(504, 280);
+            this.btnRemoveEntry.Location = new System.Drawing.Point(504, 431);
+            this.btnRemoveEntry.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.btnRemoveEntry.Name = "btnRemoveEntry";
-            this.btnRemoveEntry.Size = new System.Drawing.Size(80, 48);
+            this.btnRemoveEntry.Size = new System.Drawing.Size(80, 74);
             this.btnRemoveEntry.TabIndex = 9;
             this.btnRemoveEntry.Text = "Şterge";
             this.btnRemoveEntry.UseVisualStyleBackColor = true;
@@ -151,9 +158,10 @@
             // 
             // btnValidateChanges
             // 
-            this.btnValidateChanges.Location = new System.Drawing.Point(616, 280);
+            this.btnValidateChanges.Location = new System.Drawing.Point(616, 431);
+            this.btnValidateChanges.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.btnValidateChanges.Name = "btnValidateChanges";
-            this.btnValidateChanges.Size = new System.Drawing.Size(75, 48);
+            this.btnValidateChanges.Size = new System.Drawing.Size(75, 74);
             this.btnValidateChanges.TabIndex = 10;
             this.btnValidateChanges.Text = "OK";
             this.btnValidateChanges.UseVisualStyleBackColor = true;
@@ -161,9 +169,9 @@
             // 
             // MainWindowForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(703, 338);
+            this.ClientSize = new System.Drawing.Size(703, 520);
             this.Controls.Add(this.btnValidateChanges);
             this.Controls.Add(this.btnRemoveEntry);
             this.Controls.Add(this.btnAddEntry);
@@ -175,6 +183,9 @@
             this.Controls.Add(this.btnApplyFilter);
             this.Controls.Add(this.tbInputFilter);
             this.Controls.Add(this.lblFilter);
+            this.Font = new System.Drawing.Font("Vani", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.Name = "MainWindowForm";
             this.Text = "Agenda";
             this.Load += new System.EventHandler(this.MainWindowForm_Load);
